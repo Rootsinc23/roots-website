92cc41f6e856e1ef51e027fbfc0b99987f4cbb1b
Rootsinc23 <kato.mopa@st.futminna.edu.ng>
2024-01-28 10:03:19 +0100

    10:03

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,71 +1,122 @@
-import React, { useState } from 'react';
-import rootsLogo from '../assets/images/roots-logo.png';
-import { FaGithub, FaTwitter, FaLinkedin, FaFacebook } from 'react-icons/fa';
-import { Link } from 'react-scroll';
+import React, { useState } from "react";
+import rootsLogo from "../assets/images/roots-logo.png";
+import { FaGithub, FaTwitter, FaLinkedin, FaFacebook } from "react-icons/fa";
+import { Link } from "react-scroll";
 
 const Footer = () => {
+  const [activeLink, setActiveLink] = useState("home");
 
-    const [activeLink, setActiveLink] = useState('home');
-
-    const handleLinkClick = (link) => {
-        setActiveLink(link);
-      };
+  const handleLinkClick = (link) => {
+    setActiveLink(link);
+  };
   return (
-    <section className=' bg-[#33323d] text-white mt-[3rem] ' >
-    <div className='sm:flex sm:items-center sm:justify-around' data-aos = "fade-left">
-        <img src={rootsLogo} alt="" width={130} className='mx-auto sm:mx-0'/>
-        <p className='text-[.7rem] hidden'>As R.O.O.T.S continues to grow, it remains dedicated to its mission of unearthing potential, solving problems, and leaving a lasting impact on the world through the power of technology. This startup is not just a company; it's a movement that aims to redefine the tech landscape and pave the way for a more innovative and sustainable future.</p>
+    <section className=" bg-[#33323d] text-white mt-[3rem] ">
+      <div
+        className="sm:flex sm:items-center sm:justify-around"
+        data-aos="fade-left"
+      >
+        <img src={rootsLogo} alt="" width={130} className="mx-auto sm:mx-0" />
+        <p className="text-[.8rem] p-7 hidden">
+          As R.O.O.T.S continues to grow, it remains dedicated to its mission of
+          unearthing potential, solving problems, and leaving a lasting impact
+          on the world through the power of technology. This startup is not just
+          a company; it's a movement that aims to redefine the tech landscape
+          and pave the way for a more innovative and sustainable future.
+        </p>
 
-        <nav
-className=' sm:flex text-center sm:items-center sm:mr-[auto] sm:gap-[1rem]'>
-         <Link to='home' smooth={true} duration={800}>
-           <p onClick={() => handleLinkClick('home')} className={activeLink === 'home' ? 'active' : ''}>Home</p>
-        </Link>
-        
-        <Link to='about' smooth={true} duration={800}>
-        <p onClick={() => handleLinkClick('about')} className={activeLink === 'about' ? 'active' : ''}>About</p>
-         </Link>
-        
+        <nav className=" sm:flex text-center sm:items-center sm:mr-[auto] sm:gap-[1rem]">
+          <Link to="home" smooth={true} duration={800}>
+            <p
+              onClick={() => handleLinkClick("home")}
+              className={activeLink === "home" ? "active" : ""}
+            >
+              Home
+            </p>
+          </Link>
 
-         <Link to='services' smooth={true} duration={800}>
-         <p onClick={() => handleLinkClick('services')} className={activeLink === 'services' ? 'active' : ''}>Services</p>
-         </Link>
-         
+          <Link to="about" smooth={true} duration={800}>
+            <p
+              onClick={() => handleLinkClick("about")}
+              className={activeLink === "about" ? "active" : ""}
+            >
+              About
+            </p>
+          </Link>
 
+          <Link to="services" smooth={true} duration={800}>
+            <p
+              onClick={() => handleLinkClick("services")}
+              className={activeLink === "services" ? "active" : ""}
+            >
+              Services
+            </p>
+          </Link>
 
-         <Link to='team' smooth={true} duration={800}>
-         <p onClick={() => handleLinkClick('team')} className={activeLink === 'team' ? 'active' : ''}>Team</p>
-         </Link>
-         
+          <Link to="team" smooth={true} duration={800}>
+            <p
+              onClick={() => handleLinkClick("team")}
+              className={activeLink === "team" ? "active" : ""}
+            >
+              Team
+            </p>
+          </Link>
 
-         <Link to='social-media' smooth={true} duration={800}>
-         <p onClick={() => handleLinkClick('social media')} className={activeLink === 'social media' ? 'active' : ''}>Social Media</p>
-         </Link>
-         
+          <Link to="social-media" smooth={true} duration={800}>
+            <p
+              onClick={() => handleLinkClick("social media")}
+              className={activeLink === "social media" ? "active" : ""}
+            >
+              Social Media
+            </p>
+          </Link>
 
-         <Link to='contact' smooth={true} duration={800}>
-         <p onClick={() => handleLinkClick('contact')} className={activeLink === 'contact' ? 'active' : ''}>Contact</p>
-         </Link>
-      </nav>
+          <Link to="contact" smooth={true} duration={800}>
+            <p
+              onClick={() => handleLinkClick("contact")}
+              className={activeLink === "contact" ? "active" : ""}
+            >
+              Contact
+            </p>
+          </Link>
+        </nav>
       </div>
-      <div className='flex gap-[1rem] py-[2rem] justify-center' data-aos = "fade-left">
-         <a href="https://github.com/your-username" target="_blank" rel="noopener noreferrer">
-        <FaGithub size={30} className='text-[#2eb435]'/>
+      <div
+        className="flex gap-[1rem] py-[2rem] justify-center"
+        data-aos="fade-left"
+      >
+        <a
+          href="https://github.com/your-username"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub size={30} className="text-[#2eb435]" />
         </a>
-         <a href="https://twitter.com/your-username" target="_blank" rel="noopener noreferrer">
-        <FaTwitter size={30} className='text-[#2eb435]'/>
+        <a
+          href="https://twitter.com/your-username"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaTwitter size={30} className="text-[#2eb435]" />
         </a>
-        <a href="https://www.linkedin.com/in/your-username" target="_blank" rel="noopener noreferrer">
-        <FaLinkedin size={30} className='text-[#2eb435]'/>
+        <a
+          href="https://www.linkedin.com/in/your-username"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin size={30} className="text-[#2eb435]" />
         </a>
-        <a href="https://www.linkedin.com/in/your-username" target="_blank" rel="noopener noreferrer">
-        <FaFacebook size={30} className='text-[#2eb435]' />
+        <a
+          href="https://www.linkedin.com/in/your-username"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaFacebook size={30} className="text-[#2eb435]" />
         </a>
       </div>
-      
-      <p className='text-center pb-[2rem]'>Designed By R.O.O.T.S</p>
+
+      <p className="text-center pb-[2rem]">Designed By R.O.O.T.S</p>
     </section>
   );
-}
+};
 
 export default Footer;
--- src/pages/team.jsx
+++ src/pages/team.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import TEAM from '../team';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
+import React, { useEffect, useState } from "react";
+import TEAM from "../team";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const Team = () => {
   useEffect(() => {
-    Aos.init({duration: 2000});
- }, [])
+    Aos.init({ duration: 2000 });
+  }, []);
 
   const [showAll, setShowAll] = useState(false);
 
@@ -15,22 +15,27 @@ const Team = () => {
   };
 
   return (
-    <section className='mt-[2rem] px-[1rem] mb-[3rem]' id='team'>
-      <h2 className='text-center text-[2rem]'>Our Team</h2>
-      <div className='w-[120px] h-[2px] bg-[purple] mx-auto mt-[.3rem]'></div>
+    <section className="mt-[2rem] px-[1rem] mb-[3rem]" id="team">
+      <h2 className="text-center text-[2rem]">Our Team</h2>
+      <div className="w-[120px] h-[2px] bg-[purple] mx-auto mt-[.3rem]"></div>
 
-      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[1rem] mt-[2rem] mb-[1rem]'>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[1rem] mt-[2rem] mb-[1rem]">
         {TEAM.map((member, index) => (
           <div
             key={member.id}
             className={`border-[1px] border-[#2eb435] rounded-[.3rem] text-center ${
-              (!showAll && index >= 4) ? 'hidden lg:block' : ''
+              !showAll && index >= 4 ? "hidden lg:block" : ""
             }`}
-           data-aos = "fade-left" >
+            data-aos="fade-left"
+          >
             <div>
-              <img src={member.img} alt="" className='w-[100px] h-[100px] object-cover rounded-full mx-auto mt-[1.5rem]' />
+              <img
+                src={member.img}
+                alt=""
+                className="w-[100px] h-[100px] object-cover rounded-full mx-auto mt-[1.5rem]"
+              />
             </div>
-            <div className='p-4 text-left '>
+            <div className="p-4 text-left ">
               <p>Name: {member.name}</p>
               <p>Track: {member.track}</p>
               <p>Role: {member.role}</p>
@@ -41,14 +46,14 @@ const Team = () => {
 
       {!showAll ? (
         <button
-          className='border-[.8px]  border-[purple] text-black py-[.5rem] px-4 rounded-[.2rem] mt-4 mx-auto block focus:outline-0 lg:hidden'
+          className="border-[.8px]  border-[purple] text-black py-[.5rem] px-4 rounded-[.2rem] mt-4 mx-auto block focus:outline-0 lg:hidden"
           onClick={toggleShowAll}
         >
           See More
         </button>
-      ) :  (
+      ) : (
         <button
-          className='border-[.8px]  border-[purple] text-black py-[.5rem] px-4 rounded-[.2rem] mt-4 mx-auto block focus:outline-0 lg:hidden '
+          className="border-[.8px]  border-[purple] text-black py-[.5rem] px-4 rounded-[.2rem] mt-4 mx-auto block focus:outline-0 lg:hidden "
           onClick={toggleShowAll}
         >
           See Less
@@ -56,6 +61,6 @@ const Team = () => {
       )}
     </section>
   );
-}
+};
 
 export default Team;
