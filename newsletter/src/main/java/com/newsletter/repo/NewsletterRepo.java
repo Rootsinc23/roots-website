bf9b1368161332c26e4c3552b424a8ab1fe99cfd
R.O.O.T.S <152678765+Rootsinc23@users.noreply.github.com>
2024-06-28 10:59:31 +0100

    Delete newsletter directory

--- newsletter/src/main/java/com/newsletter/repo/NewsletterRepo.java
+++
@@ -1,8 +0,0 @@
-package com.newsletter.repo;
-
-import com.newsletter.model.Newsletter;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewsletterRepo extends JpaRepository<Newsletter, Long> {
-}
-
--- newsletter/src/main/java/com/newsletter/repo/SubscriberRepo.java
+++
@@ -1,7 +0,0 @@
-package com.newsletter.repo;
-
-import com.newsletter.model.Subscribers;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubscriberRepo extends JpaRepository<Subscribers, Long> {
-}
